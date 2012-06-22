884595397803f94bd0398803e579e1171606d3cc
Hadrien Jouet <hadrien.jouet@grownseed.net>
2012-06-21 22:10:47 -0700

    Forgot to remove old GNU/GPL headers - doh!

--- Source/moo3dfx.js
+++ Source/moo3dfx.js
@@ -1,22 +1,4 @@
 /*
-Moo3DFx - MooTools Fx based class for Moo3D
-Copyright (C) 2010 Hadrien Jouet @ Grownseed
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*
  ---
  description: Moo3DFx - MooTools Fx based class for Moo3D
 
